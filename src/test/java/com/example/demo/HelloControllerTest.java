bc1a4ab338a0f4f4c066ca15db76ba12bfc8a273
Chiradip <Chiradip.maji@thoughtworks.com>
2023-02-12 22:22:06 +0530

    Adds unit test

---
+++ src/test/java/com/example/demo/HelloControllerTest.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class HelloControllerTest {
+
+    @Test
+    void should_return_hello_world() {
+        HelloController helloController = new HelloController();
+        Assertions.assertTrue(helloController.hello().contains("Hello World"));
+    }
+}
